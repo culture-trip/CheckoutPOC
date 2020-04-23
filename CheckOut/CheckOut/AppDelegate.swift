eff89f578cfb4779b8efad3ec5ed0aa1969aa125
Warrd Adlani <warrdadlani@gmail.com>
2020-04-23 14:19:16 +0100

    house keeping

--- CheckOut/CheckOut/AppDelegate.swift
+++ CheckOut/CheckOut/AppDelegate.swift
@@ -14,15 +14,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
         // Override point for customization after application launch.
         
         window = UIWindow(frame: UIScreen.main.bounds)
+        
         let storyboard = UIStoryboard(name: "Main", bundle: .main)
         let viewController = storyboard.instantiateViewController(identifier: "TableViewController")
         let navigationController = UINavigationController(rootViewController: viewController)
+        
         navigationController.title = "JSON Configured layout"
+        
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
+        
         return true
     }
 }
