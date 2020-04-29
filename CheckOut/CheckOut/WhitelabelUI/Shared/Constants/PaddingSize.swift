7af3233c2f779e58303583e91340b7917482ab0a
Warrd Adlani <warrdadlani@gmail.com>
2020-04-29 19:30:08 +0100

    added more padding heights

--- CheckOut/CheckOut/WhitelabelUI/Shared/Constants/PaddingSize.swift
+++ CheckOut/CheckOut/WhitelabelUI/Shared/Constants/PaddingSize.swift
@@ -5,19 +5,23 @@ import UIKit
 
 public enum PaddingSize: String, Decodable {
     
-    case none = "none"
-    case small = "small"
-    case medium = "medium"
-    case large = "large"
-    case xtraLarge = "extra_large"
+    case none
+    case extraSmall = "extra_small"
+    case small
+    case medium
+    case large
+    case extraLarge = "extra_large"
+    case largest
     
     func convertToCGFloat() -> CGFloat {
         switch self {
         case .none: return 0.0
+        case .extraSmall: return 2.0
         case .small: return 4.0
         case .medium: return 8.0
         case .large: return 16.0
-        case .xtraLarge: return 32.0
+        case .extraLarge: return 32.0
+        case .largest: return 64.0
         }
     }
 }
